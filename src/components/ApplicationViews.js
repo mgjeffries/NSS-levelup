99ae8b3dccf3ea132d9948767a8b8cb4519b5aaf
mgjeffries <gib.jeffries@gmail.com>
2020-11-04 16:45:49 -0600

    add a form to create games

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,25 +1,33 @@
-import React from "react"
-import { Route } from "react-router-dom"
-import { EventList } from "./event/EventList"
-import { EventProvider } from "./event/EventProvider"
-import { GameList } from "./game/GameList"
-import { GameProvider } from "./game/GameProvider"
+import React from "react";
+import { Route } from "react-router-dom";
+import { EventList } from "./event/EventList";
+import { EventProvider } from "./event/EventProvider";
+import { GameForm } from "./game/GameForm";
+import { GameList } from "./game/GameList";
+import { GameProvider } from "./game/GameProvider";
 
-export const ApplicationViews = () => {
-    return <>
-        <GameProvider>
-            <Route exact path="/">
-                <GameList />
-            </Route>
-            <Route exact path="/games">
-                <GameList />
-            </Route>
-        </GameProvider>
+export const ApplicationViews = (props) => {
+  return (
+    <>
+      <GameProvider>
+        <Route exact path="/">
+          <GameList {...props} />
+        </Route>
+        <Route exact path="/games">
+          <GameList {...props} />
+        </Route>
+        <Route
+          exact
+          path="/games/new"
+          render={(props) => <GameForm {...props} />}
+        />
+      </GameProvider>
 
-        <EventProvider>
-            <Route exact path="/events">
-                <EventList />
-            </Route>
-        </EventProvider>
+      <EventProvider>
+        <Route exact path="/events">
+          <EventList />
+        </Route>
+      </EventProvider>
     </>
-}
+  );
+};
--- src/components/game/GameList.js
+++ src/components/game/GameList.js
@@ -1,25 +1,40 @@
-import React, { useContext, useEffect } from "react"
-import { GameContext } from "./GameProvider.js"
-import "./game.css"
+import React, { useContext, useEffect } from "react";
+import { GameContext } from "./GameProvider.js";
+import "./game.css";
 export const GameList = (props) => {
-    const { games, getGames } = useContext(GameContext)
+  const { games, getGames } = useContext(GameContext);
 
-    useEffect(() => {
-        getGames()
-    }, [])
+  useEffect(() => {
+    getGames();
+  }, []);
 
-    return (
-        <article className="games">
-            <h2>Games:</h2>
-            {
-                games.map(game => {
-                    return <section key={`game--${game.id}`} className="game">
-                        <h3 className="game__title">{game.title} by {game.maker}</h3>
-                        <div className="game__players">{game.number_of_players} players needed</div>
-                        <div className="game__skillLevel">Skill level is {game.skill_level}</div>
-                    </section>
-                })
-            }
-        </article>
-    )
-}
+  return (
+    <article className="games">
+      <button
+        className="btn btn-2 btn-sep icon-create"
+        onClick={() => {
+          props.history.push({ pathname: "/games/new" });
+        }}
+      >
+        Register New Game
+      </button>
+
+      <h2>Games:</h2>
+      {games.map((game) => {
+        return (
+          <section key={`game--${game.id}`} className="game">
+            <h3 className="game__title">
+              {game.title} by {game.maker}
+            </h3>
+            <div className="game__players">
+              {game.number_of_players} players needed
+            </div>
+            <div className="game__skillLevel">
+              Skill level is {game.skill_level}
+            </div>
+          </section>
+        );
+      })}
+    </article>
+  );
+};
