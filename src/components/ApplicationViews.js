1e0715af93aea8d21295cd34fafd819add34af7e
mgjeffries <gib.jeffries@gmail.com>
2020-11-04 10:33:38 -0600

    add list of games to application views

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,14 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { GameList } from "./game/GameList"
+import { GameProvider } from "./game/GameProvider"
 
 export const ApplicationViews = () => {
     return <>
-        <main style={{
-            margin: "5rem 2rem",
-            backgroundColor: "lightgoldenrodyellow"
-        }}>
-            Application views
-        </main>
+        <GameProvider>
+            <Route exact path="/">
+                <GameList />
+            </Route>
+        </GameProvider>
     </>
 }
