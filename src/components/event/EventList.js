2f5733e46841209c488060cecdd786dc0253daee
mgjeffries <gib.jeffries@gmail.com>
2020-11-04 12:00:36 -0600

    style events

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from "react"
 import { EventContext } from "./EventProvider.js"
+import "./event.css"
 
 export const EventList = (props) => {
     const { events, getEvents } = useContext(EventContext)
@@ -11,12 +12,12 @@ export const EventList = (props) => {
     return (
         <article className="events">
             <header className="events__header">
-                <h1>Level Up Game Events</h1>
+                <h2>Events</h2>
             </header>
             {
                 events.map(event => {
                     return <section key={event.id} className="registration">
-                        <div className="registration__game">{event.game.title}</div>
+                        <h3 className="registration__game">{event.game.title}</h3>
                         <div>{event.description}</div>
                         <div>
                             {
