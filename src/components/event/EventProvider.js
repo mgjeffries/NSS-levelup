43dec70c8a5ce64891b76ef0d14a0d1e299fead7
mgjeffries <gib.jeffries@gmail.com>
2020-11-05 12:06:29 -0600

    fix header in event provider

--- src/components/event/EventProvider.js
+++ src/components/event/EventProvider.js
@@ -19,6 +19,7 @@ export const EventProvider = (props) => {
     return fetch("http://localhost:8000/events", {
       method: "POST",
       headers: {
+        "Content-Type": "application/json",
         Authorization: `Token ${localStorage.getItem("lu_token")}`,
       },
       body: JSON.stringify(event),
